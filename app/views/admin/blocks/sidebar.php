da6ad836a4b5f472d533e9c3dfb113820b93d156
Zenfection <kietgolx65234@gmail.com>
2022-11-12 23:15:00 +0700

    fix image

--- app/views/admin/blocks/sidebar.php
+++ app/views/admin/blocks/sidebar.php
@@ -2,7 +2,7 @@
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
-            <img src="<?php echo _WEB_ROOT; ?>/assets/admin/images/logo-icon.png" class="logo-icon" alt="logo icon">
+            <img src="<?php echo _GIT_SOURCE; ?>/assets/admin/images/logo-icon.png" class="logo-icon" alt="logo icon">
         </div>
         <div>
             <h4 class="logo-text">ZenAdmin</h4>
