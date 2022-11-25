973452b7f9e0a027d3707110b3571ff32c7cd7f6
Zenfection <kietgolx65234@gmail.com>
2022-11-26 00:15:37 +0700

    update

--- app/views/layouts/admin_layout.php
+++ app/views/layouts/admin_layout.php
@@ -17,18 +17,18 @@
 	<link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/nice-select2@2.0.0/dist/css/nice-select2.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/apexcharts@3.36.1/dist/apexcharts.min.css"></script>
-	<link href="<?php echo _WEB_ROOT; ?>/assets/admin/plugins/Drag-And-Drop/dist/imageuploadify.min.css" rel="stylesheet" />
-	<link href="<?php echo _WEB_ROOT; ?>/assets/admin/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+	<link href="<?php echo _CDN_JSDelivr; ?>/assets/admin/plugins/Drag-And-Drop/dist/imageuploadify.min.css" rel="stylesheet" />
+	<link href="<?php echo _CDN_JSDelivr; ?>/assets/admin/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
 	
 	<!-- //? loader -->
-	<link href="<?php echo _WEB_ROOT; ?>/assets/admin/css/pace.min.css" rel="stylesheet" />
+	<link href="<?php echo _CDN_JSDelivr; ?>/assets/admin/css/pace.min.css" rel="stylesheet" />
 	<script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
 
 	<!-- //? Bootstrap CSS -->
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"/>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-	<link href="<?php echo _WEB_ROOT; ?>/assets/admin/css/bootstrap-extended.min.css" rel="stylesheet" />
-	<link href="<?php echo _WEB_ROOT; ?>/assets/admin/css/app.min.css" rel="stylesheet" />
+	<link href="<?php echo _CDN_JSDelivr; ?>/assets/admin/css/bootstrap-extended.min.css" rel="stylesheet" />
+	<link href="<?php echo _CDN_JSDelivr; ?>/assets/admin/css/app.min.css" rel="stylesheet" />
 	<script src="<?php echo _WEB_ROOT; ?>/cdn/js/icon-zen.min.js"></script>
 	<!-- <link rel="stylesheet" href="<?php echo _WEB_ROOT; ?> assets/admin/css/icons.min.css"> -->
 
@@ -59,9 +59,9 @@
     <script src="https://cdn.jsdelivr.net/npm/lobibox@1.2.7/js/notifications.min.js"></script>
 	
 
-	<script src="<?php echo _WEB_ROOT; ?>/assets/admin/plugins/Drag-And-Drop/dist/imageuploadify.min.js"></script>
-	<script src="<?php echo _WEB_ROOT; ?>/assets/admin/plugins/datatable/js/jquery.dataTables.min.js"></script>
-	<script src="<?php echo _WEB_ROOT; ?>/assets/admin/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
+	<script src="<?php echo _CDN_JSDelivr; ?>/assets/admin/plugins/Drag-And-Drop/dist/imageuploadify.min.js"></script>
+	<script src="<?php echo _CDN_JSDelivr; ?>/assets/admin/plugins/datatable/js/jquery.dataTables.min.js"></script>
+	<script src="<?php echo _CDN_JSDelivr; ?>/assets/admin/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
 	<!--app JS-->
 	<script src="<?php echo _WEB_ROOT; ?>/assets/admin/js/main.js"></script>
 	<script src="<?php echo _WEB_ROOT; ?>/assets/admin/js/app.js"></script>
