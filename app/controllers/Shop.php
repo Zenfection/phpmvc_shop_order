d531727da797622e6d59ecf62d6b6cf65d6875f3
Zenfection <kietgolx65234@gmail.com>
2022-12-09 15:24:57 +0700

    update

--- app/controllers/Shop.php
+++ app/controllers/Shop.php
@@ -41,15 +41,15 @@ class Shop extends Controller
     }
 
     public function index(){
-        $this->runFrist();
-        $this->data['content'] = 'shop/index';
+        // $this->runFrist();
+        // $this->data['content'] = 'shop/index';
 
-        $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 9;
-        $links = (isset($_GET['links'])) ? $_GET['links'] : 7;
-        $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
-        $this->data['sub_content']['limit'] = $limit;
-        $this->data['sub_content']['page'] = $page;
-        $this->data['sub_content']['links'] = $links;
+        // $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 9;
+        // $links = (isset($_GET['links'])) ? $_GET['links'] : 7;
+        // $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
+        // $this->data['sub_content']['limit'] = $limit;
+        // $this->data['sub_content']['page'] = $page;
+        // $this->data['sub_content']['links'] = $links;
 
         $paginator = new Paginator($this->data['sub_content']['product']);
         echo "<pre>";
